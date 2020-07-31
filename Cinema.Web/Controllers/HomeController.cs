807b982d800953d81894a17cca3c75c21e31f0ad
baongan <buingan93@gmail.com>
2020-07-31 08:32:32 +0700

    ngan

---
+++ Cinema.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cinema.Web.Models;
+using Cinema.Web.Ultilities;
+using Cinema.Web.Models.Film;
+
+namespace Cinema.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Cinema NPT";
+            return View();
+        }
+
+        public JsonResult Gets()
+        {
+            var films = new List<Film>();
+            films = ApiHelper<List<Film>>.HttpGetAsync($"{Helper.ApiUrl}api/RoomFilm/Home");
+            return Json(new { films });
+        }
+    }
+}
---
+++ Cinema.Web/wwwroot/js/home.js
@@ -0,0 +1,32 @@
+﻿var home = {} || home;
+
+home.drawTable = function () {
+    $.ajax({
+        url: "/Home/Gets",
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#tbDepart tbody').empty();
+            $.each(data.films, function (i, v) {
+                $('#tbDepart tbody').append(
+                    `<tr>
+                        <td>${v.filmId}</td>
+                        <td>${v.filmName}</td>
+                        <td>${v.title}</td>
+                         <td>${v.description}</td>
+                        <td>${v.linkTrailer}</td>
+                        <td>${v.image}</td>
+                    </tr>`
+                );
+            });
+        }
+    });
+
+};
+home.init = function () {
+    home.drawTable();
+};
+
+$(document).ready(function () {
+    home.init();
+});
