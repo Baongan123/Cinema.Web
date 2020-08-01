b68effa7f89f62cd66413d828138d634b992486e
thuong <hathuong085@gmail.com>
2020-08-01 10:58:49 +0700

    web

---
+++ Cinema.Web/wwwroot/js/JavaScript.js
@@ -0,0 +1,29 @@
+﻿var categoryfilm = {} || categoryfilm;
+
+categoryfilm.drawTable = function () {
+    $.ajax({
+        url: "/Home/Gets",
+        method: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('#tbDepart tbody').empty();
+            $.each(data.categories, function (i, v) {
+                $('#tbDepart tbody').append(
+                    `<tr>
+                        <td>${v.categoryId}</td>
+                        <td>${v.categoryName}</td>
+                        <td>${v.count}</td>
+                    </tr>`
+                );
+            });
+        }
+    });
+
+};
+categoryfilm.init = function () {
+    categoryfilm.drawTable();
+};
+
+$(document).ready(function () {
+    categoryfilm.init();
+});
